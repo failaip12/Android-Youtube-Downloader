4da100e4e013d1d47e8ee7b4b66e0bc66e913f09
failaip12 <86018517+failaip12@users.noreply.github.com>
2023-06-08 02:32:56 +0200

    Some refactoring.

--- app/src/main/java/com/example/projekat/DownloadActivity.java
+++ app/src/main/java/com/example/projekat/DownloadActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,56 +33,59 @@ import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 public class DownloadActivity extends AppCompatActivity {
 
-    Button downloadButton;
-    ProgressBar downloadProgressBar;
-    ImageView thumbnailImageView;
-    TextView titleTextView;
-    TextView lengthTextView;
-    RadioGroup qualityRadioGroup;
+    // Views
+    private ImageView thumbnailImageView;
+    private TextView titleTextView;
+    private TextView lengthTextView;
+    private ProgressBar downloadProgressBar;
+    private Button downloadButton;
+    private LinearLayout qualitiesLayout;
 
-    String ytLink;
-    String length;
-    String title;
-    String thumbnailURL;
-    ArrayList<Stream> streams_video;
-    ArrayList<Stream> streams_audio;
-    ArrayList<Stream> streams;
-    static Stream selectedStream;
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-    DownloadHandler downloadHandler;
-    ActivityResultLauncher<Intent> directoryPickerLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            if (data != null && data.getData() != null) {
-                                Uri treeUri = data.getData();
-                                DocumentFile pickedDir = DocumentFile.fromTreeUri(this, treeUri);
-                                if (pickedDir != null) {
-                                    downloadHandler.download(treeUri);
-                                }
-                            }
-                        }
-                    });
+    // YouTube data
+    private String ytLink;
+    private String length;
+    private String title;
+    private String thumbnailURL;
+    private ArrayList<Stream> streams_video;
+    private ArrayList<Stream> streams_audio;
+    private ArrayList<Stream> streams;
+    private static Stream selectedStream;
 
+    // Executor for background tasks
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private DownloadHandler downloadHandler;
+
+    // Activity result launcher for directory picker
+    private ActivityResultLauncher<Intent> directoryPickerLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
-        // Get references to layout views
+
+        initializeViews();
+        extractYouTubeData();
+        initializeDirectoryPickerLauncher();
+        downloadButton.setEnabled(false);
+
+        downloadHandler = new DownloadHandler(this);
+        downloadButton.setOnClickListener(view -> pickDirectoryForDownload());
+    }
+
+    private void initializeViews() {
         thumbnailImageView = findViewById(R.id.thumbnailImageView);
         titleTextView = findViewById(R.id.titleTextView);
         lengthTextView = findViewById(R.id.lengthTextView);
-
         downloadProgressBar = findViewById(R.id.downloadProgressBar);
+        downloadButton = findViewById(R.id.downloadButton);
+        qualitiesLayout = findViewById(R.id.qualitiesLayout);
+    }
 
-        downloadButton = findViewById(R.id.parseButton);
-        downloadButton.setEnabled(false);
+    private void extractYouTubeData() {
         ytLink = getIntent().getStringExtra("link");
-        downloadHandler = new DownloadHandler(this);
         executor.execute(() -> {
             try {
                 Youtube yt = new Youtube(ytLink);
@@ -95,146 +99,155 @@ public class DownloadActivity extends AppCompatActivity {
                 filters.put("onlyVideo", "true");
                 streams_video = yt.streams().filter(filters).orderBy("size").getAll();
                 Collections.reverse(streams_video);
+
                 filters.clear();
                 filters.put("type", "audio");
                 filters.put("onlyAudio", "true");
                 streams_audio = yt.streams().filter(filters).orderBy("abr").getAll();
                 Collections.reverse(streams_audio);
+
                 streams = new ArrayList<>();
                 streams.addAll(streams_video);
                 streams.addAll(streams_audio);
                 runOnUiThread(() -> {
-                    titleTextView.setText(title);
-                    lengthTextView.setText(length);
-                    Glide.with(this).load(thumbnailURL).into(thumbnailImageView);
-
-                    LinearLayout qualitiesLayout = findViewById(R.id.qualitiesLayout);
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-                    int margin = 18;
-                    layoutParams.setMargins(margin, margin, margin, margin);
-                    int i = 0;
-                    // Create the RadioGroup for video streams
-                    qualityRadioGroup = new RadioGroup(this);
-                    qualityRadioGroup.setLayoutParams(layoutParams);
-                    qualityRadioGroup.setOrientation(LinearLayout.VERTICAL);
-                    for (Stream stream : streams_video) {
-                        // Create the quality layout
-                        LinearLayout qualityLayout = new LinearLayout(this);
-                        qualityLayout.setLayoutParams(layoutParams);
-                        qualityLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-                        // Create the radio button
-                        RadioButton radioButton = new RadioButton(this);
-                        radioButton.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        radioButton.setPadding(16, 0, 16, 0);
-                        radioButton.setText(stream.getResolution());
-                        radioButton.setTag(stream);
-
-                        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                            if (isChecked) {
-                                selectedStream = (Stream) buttonView.getTag(); // Get the selected stream from the tag
-                                uncheckOtherRadioButtons(qualitiesLayout, radioButton);
-                                downloadButton.setEnabled(true);
-                            }
-                        });
-
-                        TextView FPSTextView = new TextView(this);
-                        FPSTextView.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        FPSTextView.setPadding(16, 0, 16, 0);
-                        FPSTextView.setText(getString(R.string.fps, String.valueOf(stream.getFps())));
-
-                        // Create the size text view
-                        TextView sizeTextView = new TextView(this);
-                        sizeTextView.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        sizeTextView.setPadding(16, 0, 16, 0);
-                        sizeTextView.setText(getString(R.string.size, String.valueOf((int) stream.getFileSize() / 1024)));
-
-                        // Add the radio button and quality text view to the quality layout
-                        qualityLayout.addView(radioButton);
-                        qualityLayout.addView(FPSTextView);
-                        qualityLayout.addView(sizeTextView);
-
-                        // Add the quality layout to the qualitiesLayout
-                        qualitiesLayout.addView(qualityLayout);
-                    }
-                    for (Stream stream : streams_audio) {
-                        // Create the quality layout
-                        LinearLayout qualityLayout = new LinearLayout(this);
-                        qualityLayout.setLayoutParams(layoutParams);
-                        qualityLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-                        // Create the radio button
-                        RadioButton radioButton = new RadioButton(this);
-                        radioButton.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        radioButton.setPadding(16, 0, 16, 0);
-                        radioButton.setText(R.string.audio_only);
-                        radioButton.setTag(stream);
-
-                        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                            if (isChecked) {
-                                selectedStream = (Stream) buttonView.getTag(); // Get the selected stream from the tag
-                                uncheckOtherRadioButtons(qualitiesLayout, radioButton);
-                                downloadButton.setEnabled(true);
-                            }
-                        });
-
-                        TextView ABRTextView = new TextView(this);
-                        ABRTextView.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        ABRTextView.setPadding(16, 0, 16, 0);
-                        ABRTextView.setText(getString(R.string.audio_bitrate, String.valueOf(stream.getAbr())));
-
-                        // Create the size text view
-                        TextView sizeTextView = new TextView(this);
-                        sizeTextView.setLayoutParams(new LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.WRAP_CONTENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                        ));
-                        sizeTextView.setPadding(16, 0, 16, 0);
-                        sizeTextView.setText(getString(R.string.size, String.valueOf((int) stream.getFileSize() / 1024)));
-
-                        // Add the radio button and quality text view to the quality layout
-                        qualityLayout.addView(radioButton);
-                        qualityLayout.addView(ABRTextView);
-                        qualityLayout.addView(sizeTextView);
-
-                        // Add the quality layout to the qualitiesLayout
-                        qualitiesLayout.addView(qualityLayout);
-                    }
-                    qualitiesLayout.addView(qualityRadioGroup);
-                    uncheckOtherRadioButtons(qualitiesLayout, null);
+                    displayYouTubeData();
+                    populateQualitiesLayout();
                 });
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         });
-        executor.shutdown();
-        // Set click listener for download button
-        downloadButton.setOnClickListener(view -> {
-                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                    intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                    intent.putExtra("ytLink", ytLink);
-                    directoryPickerLauncher.launch(intent);
-                }
-        );
     }
 
+    private void displayYouTubeData() {
+        titleTextView.setText(title);
+        lengthTextView.setText(length);
+        Glide.with(this).load(thumbnailURL).into(thumbnailImageView);
+    }
+
+    private void populateQualitiesLayout() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        int margin = 18;
+        layoutParams.setMargins(margin, margin, margin, margin);
+
+        // Create the RadioGroup for video streams
+        RadioGroup qualityRadioGroup = new RadioGroup(this);
+        qualityRadioGroup.setLayoutParams(layoutParams);
+        qualityRadioGroup.setOrientation(LinearLayout.VERTICAL);
+
+        for (Stream stream : streams_video) {
+            LinearLayout qualityLayout = createQualityLayout(layoutParams);
+            RadioButton radioButton = createVideoRadioButton(stream);
+            qualityLayout.addView(radioButton);
+            qualityLayout.addView(createFPSTextView(stream));
+            qualityLayout.addView(createSizeTextView(stream));
+            qualitiesLayout.addView(qualityLayout);
+        }
+
+        for (Stream stream : streams_audio) {
+            LinearLayout qualityLayout = createQualityLayout(layoutParams);
+            RadioButton radioButton = createAudioRadioButton(stream);
+            qualityLayout.addView(radioButton);
+            qualityLayout.addView(createABRTextView(stream));
+            qualityLayout.addView(createSizeTextView(stream));
+            qualitiesLayout.addView(qualityLayout);
+        }
+
+        qualitiesLayout.addView(qualityRadioGroup);
+        uncheckOtherRadioButtons(qualitiesLayout, null);
+    }
+
+    private LinearLayout createQualityLayout(LinearLayout.LayoutParams layoutParams) {
+        LinearLayout qualityLayout = new LinearLayout(this);
+        qualityLayout.setLayoutParams(layoutParams);
+        qualityLayout.setOrientation(LinearLayout.HORIZONTAL);
+        qualityLayout.setGravity(Gravity.CENTER_VERTICAL);
+        return qualityLayout;
+    }
+
+    private RadioButton createVideoRadioButton(Stream stream) {
+        RadioButton radioButton = new RadioButton(this);
+        radioButton.setPadding(16, 0, 16, 0);
+        radioButton.setText(stream.getResolution());
+        radioButton.setTag(stream);
+        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedStream = (Stream) buttonView.getTag();
+            }
+        });
+        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedStream = (Stream) buttonView.getTag(); // Get the selected stream from the tag
+                uncheckOtherRadioButtons(qualitiesLayout, radioButton);
+                downloadButton.setEnabled(true);
+            }
+        });
+        return radioButton;
+    }
+
+    private RadioButton createAudioRadioButton(Stream stream) {
+        RadioButton radioButton = new RadioButton(this);
+        radioButton.setPadding(16, 0, 16, 0);
+        radioButton.setText(R.string.audio_only);
+        radioButton.setTag(stream);
+        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedStream = (Stream) buttonView.getTag();
+            }
+        });
+        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                selectedStream = (Stream) buttonView.getTag(); // Get the selected stream from the tag
+                uncheckOtherRadioButtons(qualitiesLayout, radioButton);
+                downloadButton.setEnabled(true);
+            }
+        });
+        return radioButton;
+    }
+
+    private TextView createFPSTextView(Stream stream) {
+        TextView fpsTextView = new TextView(this);
+        fpsTextView.setPadding(16, 0, 16, 0);
+        fpsTextView.setText(getString(R.string.fps, String.valueOf(stream.getFps())));
+        return fpsTextView;
+    }
+
+    private TextView createSizeTextView(Stream stream) {
+        TextView sizeTextView = new TextView(this);
+        sizeTextView.setPadding(16, 0, 16, 0);
+        sizeTextView.setText(getString(R.string.size, String.valueOf((int) stream.getFileSize() / 1024)));
+        return sizeTextView;
+    }
+
+    private TextView createABRTextView(Stream stream) {
+        TextView abrTextView = new TextView(this);
+        abrTextView.setPadding(16, 0, 16, 0);
+        abrTextView.setText(getString(R.string.audio_bitrate, String.valueOf(stream.getAbr())));
+        return abrTextView;
+    }
+
+    private void pickDirectoryForDownload() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        directoryPickerLauncher.launch(intent);
+    }
+
+    private void initializeDirectoryPickerLauncher() {
+        directoryPickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri treeUri = data.getData();
+                            DocumentFile pickedDir = DocumentFile.fromTreeUri(this, treeUri);
+                            if (pickedDir != null) {
+                                downloadHandler.download(treeUri);
+                            }
+                        }
+                    }
+                });
+    }
     private static String getPathFromUri(Context context, Uri uri) {
         String documentId = DocumentsContract.getTreeDocumentId(uri);
         String[] parts = documentId.split(":");
@@ -244,7 +257,6 @@ public class DownloadActivity extends AppCompatActivity {
             return null;
         }
     }
-
     private static class DownloadHandler {
         private final Context context;
 
@@ -284,5 +296,11 @@ public class DownloadActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executor.shutdownNow();
     }
 }
